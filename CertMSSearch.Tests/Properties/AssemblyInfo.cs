37728f6f4165ac4bffbbe3cc65139bd5ce617645
Andrei Precup <andreiprecup69@gmail.com>
2017-11-15 22:56:47 +0200

    Initial Commit

---
+++ CertMSSearch.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CertMSSearch.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CertMSSearch.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ba79a1d-0382-4711-b274-2858af1f3ebb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CertMSSearch.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace CertMSSearch.Tests
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void TestMethod1()
+		{
+		}
+	}
+}
